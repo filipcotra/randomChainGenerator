18d33867ed342edadd1e68742ed8b2795d7fa065
FCotra <theheadlesswaffle@gmail.com>
2024-06-18 14:03:24 -0400

    Pretty Much Done
    - This simulation meets all the discussed requirements. Pending approval, but this should work.

--- populateContacts.py
+++ populateContacts.py
@@ -2,10 +2,14 @@ from calculateDistance import calculateDistance;
 
 # Defining contact distance limit as being 6 angstroms.
 CONTACT_LIMIT = 6;
+# Tracking the contact set key. This number is essentially
+# meaningless.
+contactKey = 0;
 
 # Method to take a particle and find all of its contacts in a
 # given particle set.
-def populateContacts(sourcePar, particleSet):
+def populateContacts(sourcePar, particleSet, contactSet):
+    global contactKey;
     distances = {};
     for par in particleSet:
         if par == sourcePar:
@@ -13,5 +17,12 @@ def populateContacts(sourcePar, particleSet):
         distance = calculateDistance(sourcePar, par);
         distances[par] = distance;
         if distance < CONTACT_LIMIT:
-            sourcePar.addContact(par);
-    return distances;
+            # Not actually adding contacts as we want to add them randomly.
+            # For now, only recording that they exist and thus should be added
+            # later in some random order. Also ensuring that each contact is
+            # only recorded once.
+            potentialVal = set([sourcePar, par]);
+            if not any(potentialVal == contactSet[x] for x in contactSet.keys()):
+                contactSet[contactKey] = set([sourcePar, par]);
+                contactKey += 1;
+    return contactSet;
--- randomChainGenerator.py
+++ randomChainGenerator.py
@@ -1,7 +1,8 @@
 from particle import *;
 from generateParticle import *;
 from populateContacts import populateContacts;
-from blobProbabilities import blobProbabilities;
+from findSeparation import findSeparation;
+import random as rand;
 
 PARTICLE_TYPES = [G_particle, P_particle, A_particle, V_particle,\
                   L_particle, I_particle, M_particle, C_particle,\
@@ -12,28 +13,31 @@ PARTICLE_TYPES = [G_particle, P_particle, A_particle, V_particle,\
 # The total amount that will be placed, not including the origin and
 # the particle immediately after.
 NUM_PARTICLES = 98;
-# Tracking results.
-inContactResults = {};
-notInContactResults = {};
-expected_inContact = 0;
-expected_noContact = 0;
-unexpected_inContact = 0;
-unexpected_noContact = 0;
+# The total number of times the simulation will be run.
+SIM_STEPS = 1000;
+# Tracking blob based stats.
+contactsByRawSeparation = {};
+contactsByBlobSeparation = {};
 
 # Repeating 1000 times.
-for i in range(1000):
+for step in range(SIM_STEPS):
     # Making an initial particle at the origin.
     particleType_0 = PARTICLE_TYPES[rand.randint(0, len(PARTICLE_TYPES) - 1)];
     particle_0 = particleType_0(xCoord = 0, yCoord = 0, zCoord = 0);
     # Making a second particle right after the first one.
     particleType_1 = PARTICLE_TYPES[rand.randint(0, len(PARTICLE_TYPES) - 1)];
     particle_0.setNext(generateParticle(particle_0, particleType_1));
+    # Adding contacts between the two particles.
+    particle_0.addContact(particle_0.next);
+    particle_0.next.addContact(particle_0);
     # Now that the first two particles exist, we can begin the loop to
     # create the N+2 particles relative to particle N. Beginning with
     # N = 0.
     currPar = particle_0;
     # Tracking all particles that have been made.
     particleSet = set([particle_0, particle_0.next])
+    # Tracking all contacts that exist.
+    contactSet = {};
     # Looping through to make N+2 particles.
     for num in range(NUM_PARTICLES):
         # Generating a random residue type.
@@ -44,49 +48,43 @@ for i in range(1000):
         currPar.next.setNext(nextPar);
         # Adding N+2 to the particle set.
         particleSet.add(nextPar);
-        # Updating the current particle to N+1.ae
+        # Updating the current particle to N+1.
         currPar = currPar.next;
+        # Adding contacts between N+1 and N+2.
+        currPar.addContact(currPar.next);
+        currPar.next.addContact(currPar);
     # Populating contacts for each particle.
     for par in particleSet:
-        distances = populateContacts(par, particleSet);
-    # Relative to each particle, find the incidence of non-contacts in a blob.
-    results = blobProbabilities(particleSet);
-    # Adding results to overall counters.
-    inContactTmp = results[0];
-    notInContactTmp = results[1];
-    expected_inContact += results[2];
-    unexpected_inContact += results[3];
-    expected_noContact += results[4];
-    unexpected_noContact += results[5];
-    for key in inContactTmp:
-        if key in inContactResults.keys():
-            inContactResults[key] = inContactResults[key] + inContactTmp[key];
+        contactSet = populateContacts(par, particleSet, contactSet);
+    # Randomly adding contacts and collecting resulting stats until
+    # all the steps have been completed.
+    numContacts = len(contactSet);
+    keySelection = None;
+    for contact in range(numContacts):
+        # Randomly selecting a contact from the existing set.
+        keySelection = rand.choice(list(contactSet.keys()));
+        newContact = list(contactSet[keySelection]);
+        par1, par2 = newContact[0], newContact[1];
+        # Gathering blob probability information based on the
+        # two contacts.
+        separation = findSeparation(par1, particleSet);
+        par2_separation = separation[par2];
+        if par2_separation in contactsByRawSeparation.keys():
+            contactsByRawSeparation[par2_separation] += 1;
         else:
-            inContactResults[key] = inContactTmp[key];
-    for key in notInContactTmp:
-        if key in notInContactResults.keys():
-            notInContactResults[key] = notInContactResults[key] + notInContactTmp[key];
+            contactsByRawSeparation[par2_separation] = 1;
+        par2_trueSeparation = par2_separation - par2.blobSize - par1.blobSize;
+        par2_blobSeparation = par2_trueSeparation if par2_trueSeparation > 0 else 0;
+        if par2_blobSeparation in contactsByBlobSeparation.keys():
+            contactsByBlobSeparation[par2_blobSeparation] += 1;
         else:
-            notInContactResults[key] = notInContactTmp[key];
-    print(f"Step {i + 1}/1000 Completed")
-# Computing some final stats.
-# 1. Calculating the contact probability by blob separation (distance - blob size).
-bySeparation_contactProbabilities = {};
-for key in notInContactResults:
-    if key in inContactResults.keys():
-        bySeparation_contactProbabilities[key] = inContactResults[key]/(inContactResults[key] + notInContactResults[key]);
-    else:
-        bySeparation_contactProbabilities[key] = 0;
-# 2. Calculating the overall contact probability.
-contactProb = (expected_inContact + unexpected_inContact)/(expected_inContact + expected_noContact + unexpected_noContact + unexpected_inContact);
-# 3. Calculating the probability of a contact being within a blob (expected contact).
-contact_expectedProb = expected_inContact/(expected_inContact + unexpected_inContact);
-# 4. Calculating the probability of a non-contact being outside of a blob (expected non-contact).
-nonContact_expectedProb = expected_noContact/(expected_noContact + unexpected_noContact);
-# 5. Calculating the probability of a result going as expected according to the blob based model.
-BBM_accuracy = (expected_inContact + expected_noContact)/(expected_inContact + expected_noContact + unexpected_inContact + unexpected_noContact);
-# Printing Results.
-print(f"Overall Contact Probability: {contactProb}");
-print(f"Probability of Contact Being Expected: {contact_expectedProb}\nProbability of Non-Contact Being Expected: {nonContact_expectedProb}");
-print(f"BBM Accuracy: {BBM_accuracy}");
-print(f"Contact Probabilties by Blob Overlap: {bySeparation_contactProbabilities}\nIn Contact Results: {inContactResults}\nNon-Contact Results: {notInContactResults}");
+            contactsByBlobSeparation[par2_blobSeparation] = 1;
+        # Making the contact in the particle set and removing
+        # it from further consideration. This is done last so as
+        # to only impact future steps.
+        par1.addContact(par2);
+        par2.addContact(par1);
+        contactSet.pop(keySelection);
+    print(f"Step {step + 1}/1000 Completed");
+print(f"Raw Separation:\n{sorted(contactsByRawSeparation.items())}");
+print(f"Blob Separation:\n{sorted(contactsByBlobSeparation.items())}");
